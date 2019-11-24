cd8cffc203cdb5ab448d7c5e1b0a5759bf2cd585
darkdemonking <55712638+darkdemonking@users.noreply.github.com>
2019-11-24 23:01:56 +0530

    Create Face detection.py

---
+++ Face detection.py
@@ -0,0 +1,32 @@
+__author__ = 'Vaibhav Punia'
+
+import cv2
+from PIL import ImageFont, ImageDraw, Image
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+if (cap.isOpened()== False):
+  print("Error opening video stream or file")
+
+while True:
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = detector.detectMultiScale(gray, 1.2,5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,0),2)
+        cv2.putText(img, 'Face', (x+70, y-20), font, 0.7, (0, 255, 5), 2)
+
+
+    cv2.putText(img, 'Press q to exit', (10, 30), font, 0.7, (0, 255, 5), 2)
+
+    cv2.imshow("frame",img)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cv2.destroyAllWindows()
